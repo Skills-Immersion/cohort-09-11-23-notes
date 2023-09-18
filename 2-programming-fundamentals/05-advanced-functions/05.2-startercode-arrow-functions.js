a4ef5e89d41124f4b1b4e87bcfee1a47179c783d
sdahal1 <raabdahl@gmail.com>
2023-09-18 08:06:48 -0700

    w2 monday am

--- 2-programming-fundamentals/05-advanced-functions/05.2-startercode-arrow-functions.js
+++ 2-programming-fundamentals/05-advanced-functions/05.2-startercode-arrow-functions.js
@@ -1,21 +1,46 @@
+//THE VALUE OF A FUNCTION CALL IS WHATEVER THAT FUNCTION CALL RETURNS! 
+
+// console.log(ratingAsText({name:"Central Park", rating: 5}))
+
 // FUNCTION DECLARATION
 function ratingAsText(park) {
   console.log("This is a function declaration.");
   return park.rating > 4 ? "Excellent!" : "Good";
 }
 
+
 // FUNCTION EXPRESSION- VARIABLE EQUALS TO A FUNCTION
+
+const ratingAsText2 = function(park) {
+  console.log("This is a function expression.");
+  return park.rating > 4 ? "Excellent!" : "Good";
+}
+
+// console.log(ratingAsText2({name:"Central Park", rating: 5}))
+
+
 
 
 // ARROW FUNCTION EXPRESSION-HOW DO WE CONVERT TO ARROW?
-  // ARROW REPLACES FUNCTION KEYWORD
+  // ARROW REPLACES FUNCTION KEYWORD -> AND PUT THE ARROW AFTER THE PARAMETERS
   // () CONTAINS THE PARAMETERS FOR THE FUNCTION
   // NO () REQUIRED IF ONLY ONE PARAMETER
 
+const ratingAsText3 = park => {
+  console.log("This is an arrow function.");
+  return park.rating > 4 ? "Excellent!" : "Good";
+}
+
+// console.log(ratingAsText3({name:"Central Park", rating: 5}))
+
 
 // IMPLICIT RETURN- WHEN THE ONLY LINE OF CODE INSIDE THE FUNCTION BODY IS A RETURN STATEMENT
+  // Remove the return word and remove the {}
+
+const ratingAsText4 = park => park.rating > 4 ? "Excellent!" : "Good";
 
 
+// console.log(ratingAsText4({name:"Central Park", rating: 5}))
 
 
 
@@ -23,6 +48,7 @@ function ratingAsText(park) {
 function getLocationCoordinates({ geo: { lat, lon } }, zoom = 10) {
   return `https://www.google.com/maps/@${lat},${lon},${zoom}z`;
 }
+
 
 const calculateCylinderVolume = function (radius, height) {
   return Math.PI * radius ** 2 * height;
--- 2-programming-fundamentals/05-advanced-functions/05.3-startercode-higher-order-functions.js
+++ 2-programming-fundamentals/05-advanced-functions/05.3-startercode-higher-order-functions.js
@@ -1,8 +1,30 @@
 const dishes = ["Calamari", "Pasta", "Lasagna", "Garlic Bread", "Salad", "Pizza"]
 // 1. GIVEN AN ARRAY OF FOOD ITEMS, PRINT EACH ITEM USING REGULAR FOR LOOP
-
+for(let i = 0; i<dishes.length; i++){
+  const element = dishes[i]
+}
 
 // 1.1 GIVEN AN ARRAY OF FOOD ITEMS, PRINT EACH ITEM USING .FOREACH()
+function printIt(element,idx){
+  console.log(element, idx);
+}
+
+// const printIt = (element, idx)=>{
+//   console.log(element, idx);
+// }
+
+// dishes.forEach(printIt)
+
+dishes.forEach((element, idx)=>{
+  // console.log(element)
+})
+
+  //printIt('Calamari', 0)
+  //printIt('Pasta', 1)
+  //printIt('Lasagna', 2)
+  //...
+
+
 
 // 2. USE FOREACH() TO LOOP THROUGH AN ARRAY OF OBJECTS AND SHOW THEIR DETAILS: "The rapper Drake earned 10000000 from rapping career with a fire level of 10."
 
@@ -24,6 +46,28 @@ const rappers = [
   }
 ];
 
+// for(let i = 0; i<rappers.length; i++){
+//   const rapperElement = rappers[i];
+//   const {name, moneyEarnedFromRapping, fireLevel} = rapperElement
+//   console.log(`The rapper ${name} earned ${moneyEarnedFromRapping} from rapping career with a fire level of ${fireLevel}.`)
+// }
+
+rappers.forEach((rapperElement)=>{
+  const {name, moneyEarnedFromRapping, fireLevel} = rapperElement
+  console.log(`The rapper ${name} earned ${moneyEarnedFromRapping} from rapping career with a fire level of ${fireLevel}.`)
+})
+
+/*
+
+//You can destructure in the parameter - here is an example 
+
+rappers.forEach(({name, moneyEarnedFromRapping, fireLevel})=>{
+  // const {name, moneyEarnedFromRapping, fireLevel} = rapperElement
+  console.log(`The rapper ${name} earned ${moneyEarnedFromRapping} from rapping career with a fire level of ${fireLevel}.`)
+})
+
+*/
+
 /* 
 GENERAL RULES OF .FOREACH()
 
@@ -36,8 +80,23 @@ GENERAL RULES OF .FOREACH()
 
 
 // 3. USE FOREACH TO ADD UP THE FIRELEVELS OF THE ARTISTS ABOVE AND GIVE BACK THE TOTALFIRE AND AVERAGEFIRE
+let total = 0;
+rappers.forEach((rapperObject,idx)=>{
+  total+= Number(rapperObject.fireLevel);
+})
+
+console.log(total)
+console.log("average is", total/rappers.length)
+
 
 // 4. USE FOREACH TO BUILD A STATEMENT FROM GIVEN DATA ABOVE "MY TOP RAPPERS LIST ARE: DRAKE, LUPE FIASCO, ROB D, AND THEY ARE ALL FUEGO.
+let sentence = "My top rappers list are: "
+rappers.forEach((rapperObject)=>{
+  sentence += `${rapperObject.name}, `
+})
+
+sentence += "and they are all fuego."
+console.log(sentence)
 
 // 5. USE FOREACH TO LOG THE MONEYEARNEDFROMRAPPING FOR EACH ARTIST ABOVE AND FORMAT USING THE DOLLAR SIGN TO SHOW AMOUNT IN DOLLARS
 
