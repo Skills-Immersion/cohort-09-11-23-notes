f7c73eb0e02a0a67eb26074ec668b32e42a8c995
sdahal1 <raabdahl@gmail.com>
2023-09-19 08:30:29 -0700

    week 2 tuesday am

--- 2-programming-fundamentals/05-advanced-functions/05.5-startercode-reduce.js
+++ 2-programming-fundamentals/05-advanced-functions/05.5-startercode-reduce.js
@@ -3,33 +3,48 @@
   -INPUT: CALLBACK FUNCTION, INITIAL VALUE
   -RETURNS: ACCUMULATED VALUE
   -CALLBACK FUNCTION DETAILS:
-    -CB PARAMETERS: ACCUMULATOR, ELEMENT AND INDEX
+    -CB PARAMETERS: ACCUMULATOR, ELEMENT, AND INDEX
     -CB RULE: RETURN AN ACCUMULATION - the callback function has to return a value that will be accumulated with the previous values on each iteration.
 */
 
 
 
 
-
 const numbers = [768, 1004.2, 433.1];
+const luckyNums = [7, 11, 3];
 
-function sumNumsForEachWay(nums){
 
+function sumNumsForEachWay(nums=[]){
+  //set an accumulator
+  let total = 0;
+  nums.forEach((num, idx)=>{
+    total += num;
+  })
+  return total
 }
 
 // console.log(sumNumsForEachWay(numbers))
 
-function sumNums(nums){
+function sumNums(nums=[]){
+  const total = nums.reduce((accumulator, element, idx)=>{
+    //accumulate to the accumulator
+    accumulator += element
+    //you have to return the accumulator at the end of this callback function
+    return accumulator;
+  },0)
 
+  return total;
 }
 
 // console.log(sumNums(numbers))
+// console.log(sumNums(luckyNums))
+
 
 
 const movies = [
   {
       title: "Austin Powers",
-      views: 1000,
+      views: '1000',
       producer: { 
           name: "Universal Studios",
           city: "Los Angeles"
@@ -56,8 +71,16 @@ const movies = [
 
 /* ADD UP ALL THE VIEWS FROM ALL THE MOVIE OBJECTS IN THE MOVIES ARRAY */
 
-function totalViews(movies){
 
+function totalViews(movies=[]){
+  const total = movies.reduce((accumulator, movieObj, idx)=>{
+    //1. accumulate to the accumulator
+    // const x = "views"
+    accumulator += movieObj.views;
+    //2. return the accumulator
+    return accumulator;
+  },0)
+  return total;
 }
 
 // console.log(totalViews(movies))
@@ -72,14 +95,32 @@ EXAMPLE OUTPUT:
   'Finding Nemo': 'Disney',
   'Forrest Gump': 'Universal Studios'
 }
+
+{
+  "Austin Powers": "Universal Studios",
+  'Finding Nemo': 'Disney',
+  'Forrest Gump': 'Universal Studios'
+  
+}
 */
+//For objects when you want to access or create a new key/value pair, use . notation or [""] notation if the key you're accessing or creating is literally spelled the way you want the property to be. If its a varaible that reperesents a property name, you use bracket notation with NO quotes
 
-function movieAndProducerName(movies){
+function movieAndProducerName(movies=[]){
+  const result = movies.reduce((acc, movieObj, idx)=>{
+    const movieTitle = movieObj.title;
+    //1. accumulate to the accumulator
+    acc[movieObj.title] = movieObj.producer.name;
+    // acc['movieTitle'] = movieObj.producer.name;
+    // acc.movieTitle = movieObj.producer.name;
+    //2. return the accumulator
+    return acc
+  },{})
 
+  return result;
 }
 
 
-console.log(movieAndProducerName(movies));
+// console.log(movieAndProducerName(movies));
 
 /* ADVANCED PROBLEM- HINT FOR YOUR ASSESSMENT:  GIVEN AN ARRAY OF MOVIE OBJECTS, RETURN AN OBJECT WHERE EACH KEY IS THE PRODUCER NAME, AND EACH VALUE IS AN ARRAY OF MOVIES ASSOCIATED WITH THAT PRODUCER NAME 
 
@@ -94,9 +135,29 @@ EXAMPLE OUTPUT:
 }
 
 
-*/
-
-function movieByProducer(movies){
+{
+  "Universal studios": [{austin powers}]
+  "Disney": [{finding nemo obj}],
 }
 
-// console.log(movieByProducer(movies))
+*/
+
+function movieByProducer(movies=[]){
+  const result = movies.reduce((acc, movieObj, idx)=>{
+    //if the accumulator at the current movie objecs producer name key does not exist, create a new key value pair and set the value to be an array
+    if(acc[movieObj.producer.name] === undefined){
+      //1. accumulate to the accumulator
+      acc[movieObj.producer.name] = [movieObj]
+    }else{
+      //if the accumulator at the current movies producer name is alreay a key whose value is an array, then push to that array
+      acc[movieObj.producer.name].push(movieObj)
+    }
+
+    //2. return the accumulator
+    return acc;
+  },{})
+
+  return result;
+}
+
+console.log(movieByProducer(movies))
--- 2-programming-fundamentals/05-advanced-functions/05.6-startercode-sort.js
+++ 2-programming-fundamentals/05-advanced-functions/05.6-startercode-sort.js
@@ -4,7 +4,7 @@
   -INPUT: CALLBACK FUNCTION
   -RETURNS: SORTED ARRAY
   -CALLBACK FUNCTION DETAILS:
-    -CB PARAMETERS: TWO ELEMENTS TO BE COMPARED
+    -CB PARAMETERS: TWO ELEMENTS TO BE COMPARED (elementA, elementB)
     -CB RULE: RETURN A NUMBER (POSITIVE, NEGATIVE, 0) - the callback function has to return a value indicating the relationship between the two elements.
       -NEGATIVE NUMBER: If the returned value is negative, the first element will be sorted before the second element. 
       -POSITIVE NUMBER: If the returned value is positive, the second element will be sorted before the first element. 
@@ -19,18 +19,24 @@
 const numbers = [23,4,12,77,-3,-5,1]
 
 //ASCENDING (LEAST TO GREATEST)
-function sortAscending(nums){
-
+function sortAscending(nums=[]){
+  nums.sort((elementA, elementB)=>{
+    return elementA - elementB
+  })
+  return nums;
 }
 
-// console.log(sortAscending(numbers))
+console.log(sortAscending(numbers))
 
 //DESCENDING
-function sortDescending(nums){
-
+function sortDescending(nums=[]){
+  nums.sort((elementA, elementB)=>{
+    return elementB - elementA;
+  })
+  return nums;
 }
 
-// console.log(sortDescending(numbers))
+console.log(sortDescending(numbers))
 
 
 /*  EXAMPLE 2: SORTING NUMBERS- GIVEN AN ARRAY OF OBJECTS */
@@ -41,11 +47,35 @@ const parks = [
     { name: "Indiana Dunes", rating: 4.1 },
   ];
 
-
+function sortDescendingParks(parks=[]){
+  parks.sort((elementA, elementB)=>{
+    return elementB.rating - elementA.rating;
+  })
+  return parks;
+}
+console.log(sortDescendingParks(parks))
 
 /* EXAMPLE 3: SORTING STRINGS */
+function sortStrings(parks=[]){
+  parks.sort((elementA, elementB)=>{
+    // if(elementA.name.toLowerCase() < elementB.name.toLowerCase()){
+    //   return -1;
+    // }else{
+    //   return 1
+    // }
+    return elementA.name.toLowerCase() < elementB.name.toLowerCase() ? -1 : 1
+  })
+  return parks;
+}
 
+console.log(sortStrings(parks));
 
 /* EXAMPLE 4: SORT STRINGS BY LENGTH */
+function sortStringsByLength(parks=[]){
+  parks.sort((elementA, elementB)=>{
+    return elementA.name.length - elementB.name.length
+  })
+  return parks;
+}
 
-
+console.log(sortStringsByLength(parks));
