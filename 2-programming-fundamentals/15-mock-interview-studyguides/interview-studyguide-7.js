4def2fc6d37205abed1ca112d78b2324fd25ed79
sdahal1 <raabdahl@gmail.com>
2023-10-10 08:13:00 -0700

    w5 d2 am

--- 2-programming-fundamentals/15-mock-interview-studyguides/interview-studyguide-7.js
+++ 2-programming-fundamentals/15-mock-interview-studyguides/interview-studyguide-7.js
@@ -12,3 +12,24 @@ Return an array of coins sorted by their price change percentage and where each 
 }
 
 */
+
+const axios = require("axios")
+const BASE_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+
+async function getAllCoins() {  
+  const response = await axios.get(`${BASE_URL}`)
+  const data = response.data
+  const sortedCoin = data.sort((a, b)=>b.price_change_percentage_24h - a.price_change_percentage_24h)
+  const formattedCoin = sortedCoin.map((coin)=>{
+    return {
+        name: coin.name,
+        current_price: coin.current_price,
+        price_change_percentage_24h: coin.price_change_percentage_24h
+    }
+  })
+  return formattedCoin.slice(0,3)
+}
+
+getAllCoins().then((response)=>{
+    console.log(response)
+})
