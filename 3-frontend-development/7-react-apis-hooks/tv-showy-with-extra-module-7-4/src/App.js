984b4334dbe1b641f3e04024070dd2474da92e72
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-10-30 10:24:48 -0700

    setup for review

---
+++ 3-frontend-development/7-react-apis-hooks/tv-showy-with-extra-module-7-4/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Show from './Show';
+
+function App() {
+  const [numClicks, setNumClicks] = useState(0);
+  const [numMouseover, setNumMouseover] = useState(0);
+
+  // set this up with the same datatype that we expect back from the API
+  const [shows, setShows] = useState([])
+
+  // write a useEffect to get the data from the api
+  useEffect(() => {
+    // make the request, transform the response data, and save the data into our state variable
+    let abortController = new AbortController();
+    fetch('https://api.tvmaze.com/shows',
+      { signal: abortController.signal })
+      .then(response => response.json())
+      .then(data => setShows(data))
+      .catch(e => {
+        if (e.name !== 'AbortError') {
+          throw e;
+        }
+      });
+    return () => {
+      console.log('we are running the cleanup function')
+      abortController.abort()
+    };
+  }, [])
+  // async/await useEffect code
+  // useEffect(() => {
+  //   async function loadTvShows() {
+  //     const response = await fetch('https://api.tvmaze.com/shows');
+  //     const data = await response.json();
+  //     setShows(data);
+  //   }
+  //   loadTvShows();
+  // }, []);
+  // by default, this useEffect gets rerun every time a state variable updates
+  // even though the state variable is not related at all
+  // or, we can pass in an array of dependencies to change that
+  useEffect(() => {
+    // inside of this callback function, we can run side-effect-ful code
+    document.title = `TV Showy${new Date().toTimeString()}`
+
+    // return a cleanup function to set the document title to something else
+    return () => document.title = 'goodbye';
+  }, [numMouseover])
+  return (
+    <div className="container">
+      <h1 onClick={() => setNumClicks(numClicks + 1)} onMouseOver={() => setNumMouseover(numMouseover + 1)}>TV Showy {numMouseover} {numClicks}</h1>
+      <ul>
+
+        {shows.map(potato => <Show show={potato} key={potato.id} />)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ 3-frontend-development/7-react-apis-hooks/tv-showy-with-extra-module-7-4/src/Show.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Show({ show }) {
+  return <div>
+    <h2>{show.name}</h2>
+    <img src={show.image.medium} />
+    <p>Average rating: {show.rating.average}</p>
+  </div>
+}
+
+export default Show;
