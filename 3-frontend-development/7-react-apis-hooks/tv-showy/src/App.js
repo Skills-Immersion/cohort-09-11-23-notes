9817c23f19b3958d632adb50b079a0cffe783a9b
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-10-30 08:16:32 -0700

    add in the abortcontroller

--- 3-frontend-development/7-react-apis-hooks/tv-showy/src/App.js
+++ 3-frontend-development/7-react-apis-hooks/tv-showy/src/App.js
@@ -10,27 +10,41 @@ function App() {
   const [shows, setShows] = useState([])
 
   // write a useEffect to get the data from the api
-  // useEffect(() => {
-  //   // make the request, transform the response data, and save the data into our state variable
-  //   fetch('https://api.tvmaze.com/shows')
-  //     .then(response => response.json())
-  //     .then(data => setShows(data));
-  // }, [])
-  // async/await useEffect code
   useEffect(() => {
-    async function loadTvShows() {
-      const response = await fetch('https://api.tvmaze.com/shows');
-      const data = await response.json();
-      setShows(data);
-    }
-    loadTvShows();
-  }, []);
+    // make the request, transform the response data, and save the data into our state variable
+    let abortController = new AbortController();
+    fetch('https://api.tvmaze.com/shows',
+      { signal: abortController.signal })
+      .then(response => response.json())
+      .then(data => setShows(data))
+      .catch(e => {
+        if (e.name !== 'AbortError') {
+          throw e;
+        }
+      });
+    return () => {
+      console.log('we are running the cleanup function')
+      abortController.abort()
+    };
+  }, [])
+  // async/await useEffect code
+  // useEffect(() => {
+  //   async function loadTvShows() {
+  //     const response = await fetch('https://api.tvmaze.com/shows');
+  //     const data = await response.json();
+  //     setShows(data);
+  //   }
+  //   loadTvShows();
+  // }, []);
   // by default, this useEffect gets rerun every time a state variable updates
   // even though the state variable is not related at all
   // or, we can pass in an array of dependencies to change that
   useEffect(() => {
     // inside of this callback function, we can run side-effect-ful code
     document.title = `TV Showy${new Date().toTimeString()}`
+
+    // return a cleanup function to set the document title to something else
+    return () => document.title = 'goodbye';
   }, [numMouseover])
   return (
     <div className="container">
