83191efc2c6ca8e5d3d0a72390806d096eab12cf
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-10-30 08:01:05 -0700

    fetch, the nice and easy way

--- 3-frontend-development/7-react-apis-hooks/tv-showy/src/App.js
+++ 3-frontend-development/7-react-apis-hooks/tv-showy/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Show from './Show';
 
 function App() {
+  const [numClicks, setNumClicks] = useState(0);
+  const [numMouseover, setNumMouseover] = useState(0);
+
+  // set this up with the same datatype that we expect back from the API
+  const [shows, setShows] = useState([])
+
+  // write a useEffect to get the data from the api
+  // useEffect(() => {
+  //   // make the request, transform the response data, and save the data into our state variable
+  //   fetch('https://api.tvmaze.com/shows')
+  //     .then(response => response.json())
+  //     .then(data => setShows(data));
+  // }, [])
+  // async/await useEffect code
+  useEffect(() => {
+    async function loadTvShows() {
+      const response = await fetch('https://api.tvmaze.com/shows');
+      const data = await response.json();
+      setShows(data);
+    }
+    loadTvShows();
+  }, []);
+  // by default, this useEffect gets rerun every time a state variable updates
+  // even though the state variable is not related at all
+  // or, we can pass in an array of dependencies to change that
+  useEffect(() => {
+    // inside of this callback function, we can run side-effect-ful code
+    document.title = `TV Showy${new Date().toTimeString()}`
+  }, [numMouseover])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 onClick={() => setNumClicks(numClicks + 1)} onMouseOver={() => setNumMouseover(numMouseover + 1)}>TV Showy {numMouseover} {numClicks}</h1>
+      <ul>
+
+        {shows.map(potato => <Show show={potato} key={potato.id} />)}
+      </ul>
     </div>
   );
 }
