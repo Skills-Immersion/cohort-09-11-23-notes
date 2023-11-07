941636cc68b0da03c0344da6b292402d41e2c6eb
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-11-07 10:52:43 -0800

    about to componentize the creature form

--- 3-frontend-development/10-flashcards-capstone/creatures/src/AddACreature.js
+++ 3-frontend-development/10-flashcards-capstone/creatures/src/AddACreature.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
+// similarities: 
+// all the labels, all the inputs
 
+// differences:
+// what to do on submit (post new creature vs put to edit an existing creature)
+// initial form data: the edit form should contain the original creature data
+
+// today's steps:
+// 1. modify the AddACreature form so that the shared component goodness is moved into a separate component
+// 2. use that separate component to also render an EditCreature form to the screen
 function AddACreature({ addCreature }) {
   const initialFormData = {
     name: '',
@@ -22,10 +31,14 @@ function AddACreature({ addCreature }) {
   }
   return <div>
     <form onSubmit={handleSubmit}>
-      <input name="name" type="text" onChange={handleChange} value={formData.name} />
-      <input name="imageUrl" type="url" onChange={handleChange} value={formData.imageUrl} />
-      <input name="abilities" type="text" onChange={handleChange} value={formData.abilities} />
-      <input name="type" type="text" onChange={handleChange} value={formData.type} />
+      <label htmlFor="name">Name</label>
+      <input id="name" name="name" type="text" onChange={handleChange} value={formData.name} />
+      <label htmlFor="imageUrl">Image URL</label>
+      <input id="imageUrl" name="imageUrl" type="url" onChange={handleChange} value={formData.imageUrl} />
+      <label htmlFor="abilities">Abilities</label>
+      <input id="abilities" name="abilities" type="text" onChange={handleChange} value={formData.abilities} />
+      <label htmlFor="type">Type</label>
+      <input id="type" name="type" type="text" onChange={handleChange} value={formData.type} />
       <input type="submit" />
     </form>
   </div>
